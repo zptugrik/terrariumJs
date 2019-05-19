268fc7288bd48ec8a4e195bc27d0158ffe713669
Dmytro Grabko <dmitrygr@2kgroup.com>
2019-05-19 21:33:39 +0300

    game is almost done

--- game.js
+++ game.js
@@ -13,6 +13,9 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var plantsPlaces;
+var level = 1;
+var handA = null;
 function preload() {
 
     this.load.image('background', 'assets/textures/backgrounds/bg_gradient.png');
@@ -24,11 +27,22 @@ function preload() {
     this.load.image('plant_level_progress_bg', 'assets/textures/ui/plant_level_progress_bg.png');
     this.load.image('buttonLevelUp', 'assets/textures/ui/btn_orange_hover.png');
     this.load.image('oxygenIcon', 'assets/textures/ui/oxygen_icon.png');
+    this.load.image('blueLine', 'assets/textures/ui/btn_blue.png');
     this.load.image('plantPlace', 'assets/textures/plant_here.png');
+    this.load.image('hand', 'assets/textures/hand.png');
+    this.load.image('pot', 'assets/textures/pot_empty.png');
+    this.load.image('snakePlant1', 'assets/textures/plants/snakeplant1_background.png');
+    this.load.image('snakePlant2', 'assets/textures/plants/snakeplant2_background.png');
+    this.load.image('snakePlant3', 'assets/textures/plants/snakeplant5_background.png');
+    this.load.image('aloePlant1', 'assets/textures/plants/aloevera3_background.png');
+    this.load.image('aloePlant2', 'assets/textures/plants/aloevera4_background.png');
+    this.load.image('aloePlant3', 'assets/textures/plants/aloevera5_background.png');
+    this.load.image('lotusPlant', 'assets/textures/plants/flamingoflower4_background.png');
+    this.load.image('popupBg', 'assets/textures/roundedRect.png');
 }
 
 function create() {
-
+    this.counter = 0;
     this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'background').setOrigin(0);
     this.background.name = "background";
     backgroundOnResize(this.background, this.scale.width, this.scale.height);
@@ -79,9 +93,21 @@ function create() {
     this.plantProgressBg = this.add.sprite(125, 20, 'plant_level_progress_bg').setOrigin(0);
     this.plantProgressBg.scaleX = 1.4;
 
+    this.plantProgressLine =  this.add.sprite(127, 20, 'blueLine').setOrigin(0);
+    this.plantProgressLine.scaleX = 0;
+    this.plantProgressLine.scaleY = 0.13;
+
     this.textLevel = this.add.text(60, 15, 'Level 1', { fontFamily: 'Arial', fontSize: 16, color: '#000000' });
 
-    this.buttonLevelUp = this.add.image(300, 7, 'buttonLevelUp').setOrigin(0);
+    this.buttonLevelUp = this.add.image(300, 7, 'buttonLevelUp').setOrigin(0).setInteractive();
+    this.buttonLevelUp.on('pointerdown', () => {
+        if(level != 2) return;
+        this.popupLevel2.visible = true;
+        level = 3;
+        this.plantProgressLine.scaleX = 0;
+        this.textLevel.setText('Level 2');
+        backgroundOnResize(this.hand, this.scale.width, this.scale.height, this.scale.orientation);
+    });
     this.buttonLevelUp.setScale(0.4);
     this.textLevelUp = this.add.text(320, 15, 'LevelUp', { fontFamily: 'Arial', fontSize: 16, color: '#FFFFFF' });
 
@@ -92,42 +118,135 @@ function create() {
     this.topBar = this.add.container(0, 0);
     this.topBar.name = "topBar";
     this.topBar.add([ this.topBarBackground, this.settingsIcon, this.currencyBg, this.plantProgressBg, this.textLevel,
-        this.buttonLevelUp, this.textLevelUp, this.oxygenIcon, this.textOxygenCounter
+        this.buttonLevelUp, this.textLevelUp, this.oxygenIcon, this.textOxygenCounter, this.plantProgressLine
     ]);
     backgroundOnResize(this.topBar, this.scale.width, this.scale.height, this.scale.orientation);
-
+    this.snakePlant = this.add.image(0, 0, 'snakePlant1').setOrigin(0);
+    this.snakePlant.visible = false;
+    this.aloePlant = this.add.image(0, 0, 'aloePlant1').setOrigin(0);
+    this.aloePlant.visible = false;
     // places for plant input
     this.plantPlace1 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
-    this.plantPlace2 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
+    this.plantPlace2 = this.add.image(0, 0, 'plantPlace').setOrigin(0).setInteractive();
+    this.plantPlace2.on('pointerdown', () => {
+        if(level != 4) return;
+        if(!this.aloePlant.visible){
+            this.plantPlace2.setTexture('pot');
+            this.plantPlace2.setPosition(this.plantPlace2.x, this.plantPlace2.y - 20);
+            this.aloePlant.setPosition(this.plantPlace2.x, this.plantPlace2.y - 10);
+            this.aloePlant.visible = true;
+        }
+        else{
+            this.counter++;
+            this.textOxygenCounter.setText(this.counter);
+            this.plantProgressLine.scaleX += 0.65/10;
+            if(this.counter == 15) {
+                this.aloePlant.setTexture('aloePlant2');
+                this.aloePlant.setPosition(this.plantPlace2.x, this.plantPlace2.y + 10);
+            }
+            if(this.counter == 20) {
+                this.aloePlant.setTexture('aloePlant3');
+                level = 5;
+                this.popupLevel3.visible = true;
+                this.hand.visible = false;
+                if(!_.isNil(handA)) handA.kill();
+                backgroundOnResize(this.popupLevel3, this.scale.width, this.scale.height, this.scale.orientation);
+                backgroundOnResize(this.hand, this.scale.width, this.scale.height, this.scale.orientation);
+            }
+        }
+    });
     this.plantPlace3 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
     this.plantPlace4 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
-    this.plantPlace5 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
+    this.plantPlace5 = this.add.image(0, 0, 'plantPlace').setOrigin(0).setInteractive();
+    this.plantPlace5.on('pointerdown', () => {
+        if(this.counter >= 10) return;
+        if(!this.snakePlant.visible){
+            this.plantPlace5.setTexture('pot');
+            this.snakePlant.setPosition(this.plantPlace5.x, this.plantPlace5.y - 40);
+            this.snakePlant.visible = true;
+        }
+        else{
+            this.counter++;
+            this.textOxygenCounter.setText(this.counter);
+            this.plantProgressLine.scaleX += 0.65/10;
+            if(this.counter == 5) this.snakePlant.setTexture('snakePlant2');
+            if(this.counter == 10) {
+                this.snakePlant.setTexture('snakePlant3');
+                level = 2;
+                backgroundOnResize(this.hand, this.scale.width, this.scale.height, this.scale.orientation);
+            }
+        }
+    });
     this.plantPlace6 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
     this.plantPlace7 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
     this.plantPlace8 = this.add.image(0, 0, 'plantPlace').setOrigin(0);
-    this.plantsPlaces = this.add.container(0, 0);
-    this.plantsPlaces.name = "plantsPlaces";
-    this.plantsPlaces.add([
+    plantsPlaces = this.add.container(0, 0);
+    plantsPlaces.name = "plantsPlaces";
+    plantsPlaces.add([this.snakePlant, this.aloePlant,
         this.plantPlace1, this.plantPlace2, this.plantPlace3, this.plantPlace4,
         this.plantPlace5, this.plantPlace6, this.plantPlace7, this.plantPlace8
-    ]);
-    backgroundOnResize(this.plantsPlaces, this.scale.width, this.scale.height, this.scale.orientation);
 
+    ]);
+    backgroundOnResize(plantsPlaces, this.scale.width, this.scale.height, this.scale.orientation);
+
+
+    this.popupBg = this.add.image(0, 0, 'popupBg').setOrigin(0);
+    this.popupBg.scaleX = 3;
+    this.popupBg.scaleY = 3.2;
+    this.textCongratulation = this.add.text(210, 54, 'CONGRATZ!', { fontFamily: 'Arial', fontSize: 64, color: '#000000' });
+    this.textReachLevel = this.add.text(100, 140, 'You reached level 2', { fontFamily: 'Arial', fontSize: 64, color: '#000000' });
+    this.textAloe = this.add.text(450, 340, 'Aloe vera', { fontFamily: 'Arial', fontSize: 58, color: '#000000' });
+    this.textSeed = this.add.text(450, 400, 'Seed', { fontFamily: 'Arial', fontSize: 48, color: '#000000' });
+    this.buttonLevel2 = this.add.image(220, 635, 'buttonLevelUp').setOrigin(0).setInteractive();
+    this.buttonLevel2.on('pointerdown', () => {
+        if(level != 3) return;
+        this.popupLevel2.visible = false;
+        level = 4;
+        backgroundOnResize(this.hand, this.scale.width, this.scale.height, this.scale.orientation);
+    });
+    this.buttonLevel2.setScale(1.4);
+    this.textLevel2 = this.add.text(283, 660, 'PLANT', { fontFamily: 'Arial', fontSize: 64, color: '#FFFFFF' });
+    this.popupLevel2PlantBg = this.add.sprite(127, 320, 'blueLine').setOrigin(0);
+    this.popupLevel2PlantBg.scaleY = 2.2;
+    this.popupLevel2Plant = this.add.sprite(140, 185, 'aloePlant1').setOrigin(0);
+    this.popupLevel2 = this.add.container(0, 0);
+    this.popupLevel2.name = "popupLevel2";
+    this.popupLevel2.visible = false;
+    this.popupLevel2.add([this.popupBg, this.textCongratulation, this.textReachLevel, this.textAloe, this.textSeed,
+        this.buttonLevel2, this.textLevel2, this.popupLevel2PlantBg, this.popupLevel2Plant]);
+    backgroundOnResize(this.popupLevel2, this.scale.width, this.scale.height, this.scale.orientation);
+
+
+    this.popupBgLevel3 = this.add.image(0, 0, 'popupBg').setOrigin(0);
+    this.popupBgLevel3.scaleX = 3;
+    this.popupBgLevel3.scaleY = 3.2;
+    this.textCongratulationLevel3 = this.add.text(210, 54, 'GREAT JOB!', { fontFamily: 'Arial', fontSize: 64, color: '#000000' });
+    this.textReachLevelLevel3 = this.add.text(200, 385, 'LOTUS\nUNLOCKED', {align:'center', fontFamily: 'Arial', fontSize: 64, color: '#000000' });
+    this.textAloeLevel3 = this.add.text(170, 620, 'Collect more plants\nin the full game!', {align:'center',  fontFamily: 'Arial', fontSize: 48, color: '#000000' });
+    this.buttonLevel3 = this.add.image(185, 875, 'buttonLevelUp').setOrigin(0).setInteractive();
+    this.buttonLevel3.on('pointerdown', () => {
+        window.location.href = "http://www.greenpandagames.com";
+    });
+    this.buttonLevel3.setScale(1.7);
+    this.textLevel3 = this.add.text(210, 915, 'CONTINUE', { fontFamily: 'Arial', fontSize: 64, color: '#FFFFFF' });
+    this.popupLevel3PlantBg = this.add.sprite(270, 135, 'blueLine').setOrigin(0);
+    this.popupLevel3PlantBg.scaleY = 2.2;
+    this.popupLevel3Plant = this.add.sprite(280, 40, 'lotusPlant').setOrigin(0);
+    this.popupLevel3 = this.add.container(0, 0);
+    this.popupLevel3.name = "popupLevel3";
+    this.popupLevel3.visible = false;
+    this.popupLevel3.add([this.popupBgLevel3, this.textCongratulationLevel3, this.textReachLevelLevel3, this.textAloeLevel3,
+        this.buttonLevel3, this.textLevel3, this.popupLevel3PlantBg, this.popupLevel3Plant]);
+    backgroundOnResize(this.popupLevel3, this.scale.width, this.scale.height, this.scale.orientation);
+
+
+
+
+    this.hand = this.add.sprite(0, 0, 'hand').setOrigin(0);
+    this.hand.name = "hand";
+    backgroundOnResize(this.hand, this.scale.width, this.scale.height, this.scale.orientation);
 
     this.scale.on('resize', resize, this);
-    /*checkOriention(this.scale.orientation);
-
-    this.scale.on('orientationchange', checkOriention, this);
-    function checkOriention (orientation)
-    {
-        if (orientation === Phaser.Scale.PORTRAIT)
-        {
-        }
-        else if (orientation === Phaser.Scale.LANDSCAPE)
-        {
-
-        }
-    }*/
 }
 
 function resize (gameSize)
@@ -148,5 +267,7 @@ function resize (gameSize)
     backgroundOnResize(this.floorRight, this.scale.width, this.scale.height, this.scale.orientation);
 
     backgroundOnResize(this.topBar, this.scale.width, this.scale.height, this.scale.orientation);
-    backgroundOnResize(this.plantsPlaces, this.scale.width, this.scale.height, this.scale.orientation);
+    backgroundOnResize(plantsPlaces, this.scale.width, this.scale.height, this.scale.orientation);
+    backgroundOnResize(this.popupLevel2, this.scale.width, this.scale.height, this.scale.orientation);
+    backgroundOnResize(this.hand, this.scale.width, this.scale.height, this.scale.orientation);
 }
--- items/background.js
+++ items/background.js
@@ -36,7 +36,34 @@ var bgConfigVO = {
              landscape: 0.26, portrait: 1
         },
         position: {
-            shift: {landscape: {x: 680, y: 4, secondRowY: 0.96}, portrait: {x: 20, y: 20, secondRowY: 0.25}}
+            shift: {landscape: {x: 680, y: 4, secondRowY: 0.93}, portrait: {x: 20, y: 20, secondRowY: 0.245}}
+        }
+    },
+    popupLevel2: {
+        scale: {
+            landscape: 0.26, portrait: 1
+        },
+        position: {
+            shift: {landscape: {x: 680, y: 4, secondRowY: 0.93}, portrait: {x: 20, y: 20, secondRowY: 0.245}}
+        }
+    },
+    popupLevel3: {
+        scale: {
+            landscape: 0.26, portrait: 1
+        },
+        position: {
+            shift: {landscape: {x: 680, y: 4, secondRowY: 0.93}, portrait: {x: 20, y: 20, secondRowY: 0.245}}
+        }
+    },
+    hand: {
+        scale:
+            {landscape: 0.26, portrait: 0.8},
+        position: {
+            level1: {landscape: {x:0.16, y:0.7}, portrait: {x:0.03, y: 0.7}},
+            level2: {landscape: {x:0.6, y:0.2}, portrait: {x:0.7, y: 0.1}},
+            level3: {landscape: {x:0.45, y:0.8}, portrait: {x:0.35, y: 0.6}},
+            level4: {landscape: {x:0.37, y:0.65}, portrait: {x:0.25, y: 0.6}},
+            level5: {landscape: {x:0.45, y:0.8}, portrait: {x:0.35, y: 0.6}}
         }
     }
 };
@@ -99,39 +126,85 @@ function backgroundOnResize(item, width, height, orientation){
             if (orientation === Phaser.Scale.PORTRAIT) {
                 item.setScale(bgConfigVO.plantsPlaces.scale.portrait);
                 _.forEach(item.list, function(value, key) {
-                    if(key > 3){
-                        value.x = bgConfigVO.plantsPlaces.position.shift.portrait.x * (2.7 + key - 4)
-                            + value.width * (key - 4);
-                        value.y = height * bgConfigVO.plantsPlaces.position.shift.portrait.secondRowY;
+                    if(key > 1){
+                        if(key > 5){
+                            value.x = bgConfigVO.plantsPlaces.position.shift.portrait.x * (1.7 + key - 5)
+                                + value.width * (key - 6);
+                            value.y = height * bgConfigVO.plantsPlaces.position.shift.portrait.secondRowY;
+                        }
+                        else{
+                            value.x = bgConfigVO.plantsPlaces.position.shift.portrait.x * (0.7 + key)
+                                + value.width * (key - 2);
+                        }
                     }
-                    else{
-                        value.x = bgConfigVO.plantsPlaces.position.shift.portrait.x * (2.7 + key)
-                            + value.width * key;
-                    }
-
                 });
-                item.y = height * bgConfigVO.floor.position["floorMiddle"].portrait.y - item.list[0].height
+                item.y = height * bgConfigVO.floor.position["floorMiddle"].portrait.y - item.list[2].height
                     - bgConfigVO.plantsPlaces.position.shift.portrait.y;
 
             }
             else if (orientation === Phaser.Scale.LANDSCAPE) {
                 item.setScale(bgConfigVO.plantsPlaces.scale.landscape);
                 _.forEach(item.list, function(value, key) {
-                    if(key > 3){
-                        value.x = bgConfigVO.plantsPlaces.position.shift.landscape.x * (1 + key - 4)
-                            + value.width * bgConfigVO.plantsPlaces.scale.landscape * (key - 4);
-                        value.y = height * bgConfigVO.plantsPlaces.position.shift.landscape.secondRowY;
+                    if(key > 1){
+                        if(key > 5){
+                            value.x = bgConfigVO.plantsPlaces.position.shift.landscape.x * (key - 5)
+                                + value.width * bgConfigVO.plantsPlaces.scale.landscape * (key - 5);
+                            value.y = height * bgConfigVO.plantsPlaces.position.shift.landscape.secondRowY;
+                        }
+                        else{
+                            value.x = bgConfigVO.plantsPlaces.position.shift.landscape.x * (key - 1)
+                                + value.width * bgConfigVO.plantsPlaces.scale.landscape * (key - 1) ;
+                        }
                     }
-                    else{
-                        value.x = bgConfigVO.plantsPlaces.position.shift.landscape.x * (1 + key)
-                            + value.width * bgConfigVO.plantsPlaces.scale.landscape * key;
-                    }
+
 
                 });
                 item.y = height * bgConfigVO.floor.position["floorMiddle"].landscape.y
-                    - item.list[0].height * bgConfigVO.plantsPlaces.scale.landscape
+                    - item.list[2].height * bgConfigVO.plantsPlaces.scale.landscape
                     - bgConfigVO.plantsPlaces.position.shift.landscape.y;
             }
+            item.list[0].setPosition(item.list[6].x, item.list[6].y - 40);
+            item.list[1].setPosition(item.list[3].x, item.list[3].y - 40);
+            break;
+        case "popupLevel2":
+        case "popupLevel3":
+            if (orientation === Phaser.Scale.PORTRAIT) {
+                item.setScale(bgConfigVO.popupLevel2.scale.portrait);
+                item.setPosition(width / 2 - 256 * 1.5, height / 2 - 2.5 * 256);
+            }
+            else if (orientation === Phaser.Scale.LANDSCAPE) {
+                item.setScale(bgConfigVO.popupLevel2.scale.landscape);
+                item.setPosition(width / 2 - 0.4 * 256, height / 2 - 0.4 * 256);
+            }
+            break;
+
+        case "hand":
+            if (orientation === Phaser.Scale.PORTRAIT) {
+                item.setScale(bgConfigVO.hand.scale.portrait);
+                if(level ==1) item.scaleY = -item.scaleY;
+                item.setPosition(width * bgConfigVO.hand.position[["level" + level]].portrait.x,
+                    height * bgConfigVO.hand.position["level" + level].portrait.y);
+                var handDestination = item.y - 200 * bgConfigVO.hand.scale.portrait;
+
+            }
+            else if (orientation === Phaser.Scale.LANDSCAPE) {
+
+                item.setScale(bgConfigVO.hand.scale.landscape);
+                if(level ==1) item.scaleY = -item.scaleY;
+                item.setPosition(width * bgConfigVO.hand.position["level" + level].landscape.x,
+                    height * bgConfigVO.hand.position["level" + level].landscape.y);
+                var handDestination = item.y - 200 * bgConfigVO.hand.scale.landscape;
+            }
+            if(!_.isNil(handA)) handA.kill();
+            handA = new TweenMax.to(item, 0.4, {
+                y: handDestination,
+                onComplete: function () {
+                    handA.reverse();
+                }, onReverseComplete: function () {
+                    handA.play();
+
+                }
+            });
             break;
     }
 
